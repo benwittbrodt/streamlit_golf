ba2093ce4707cd7be6cea88b2c01c7836fdfe496
Ben Wittbrodt <benwitt08@gmail.com>
2024-01-10 22:08:03 -0600

    Update community_app.py

--- community_app.py
+++ community_app.py
@@ -172,3 +172,39 @@ def performance_by_par(filter):
   
 
     return plot
+
+# Initialize session state variable
+if 'driving_acc_toggle' not in st.session_state:
+    st.session_state['driving_acc_toggle'] = 0
+
+st.title("Garmin Golf Data Analysis")
+
+# Display gap analysis 
+gap_analysis = distance_per_club()
+st.plotly_chart(gap_analysis)
+st.caption("Analysis of the gapping via box plot for golf shots. Ideally each club should occupy its' own range of distance")
+
+st.header("Driving Accuray from the Tee")
+st.text("Did I hit the fairway?")
+fig, fig1 = driving_accuracy()
+
+# Render the graph above the toggle
+if st.session_state['driving_acc_toggle'] == 1:
+    st.plotly_chart(fig1)
+else: 
+    st.plotly_chart(fig)
+
+# Toggle for switching between percentage view and normal view
+togg = st.toggle('View by percentage', key='driving_acc_toggle')
+
+st.divider()
+
+st.header("Scoring Performance by Hole Par Rating")
+tab1, tab2, tab3, tab4 = st.tabs(["Overall","Par 3", "Par 4", "Par 5"])
+tab_write = [tab1, tab2, tab3, tab4]
+z = 0
+for i in [None,3,4,5]:
+    plot = performance_by_par(i)
+
+    tab_write[z].plotly_chart(plot)
+    z+=1
